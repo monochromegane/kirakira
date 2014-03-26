0fd6fd05cca688dbe5aa2e27db3f1b6afad6b5b2
monochromegane <dev.kuro.obi@gmail.com>
2014-03-27 03:04:50 +0900

    Implemented kirakira plugin.

--- lib/kirakira.rb
+++ lib/kirakira.rb
@@ -1,5 +1,21 @@
 require "kirakira/version"
+require "string-irc"
 
-module Kirakira
-  # Your code goes here...
+class Kirakira
+  include Cinch::Plugin
+
+  ANSI_COLORS = [:white, :blue, :green, :red, :brown, :purple, :orange, :yellow, :light_green, :teal, :light_cyan, :light_blue, :pink]
+
+  match(/^k[ 　](.+?)[ 　](.+)/, method: :send_kirakira_msg, use_prefix: false)
+
+  def send_kirakira_msg(m, channel, msg)
+    Channel("#" + channel).join
+    Channel("#" + channel).send("#{m.user.nick}: #{to_kirakira(msg)}")
+  end
+
+  def to_kirakira(str)
+    colored = str.chars.map {|ch| StringIrc.new(ch).send(ANSI_COLORS.sample)}
+    return colored.join("")
+  end
+
 end
--- lib/kirakira/version.rb
+++ lib/kirakira/version.rb
@@ -1,3 +1,3 @@
-module Kirakira
+class Kirakira
   VERSION = "0.0.1"
 end
